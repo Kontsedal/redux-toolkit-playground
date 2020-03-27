d4ec2d972159527219fbafa77cdabfa315048a60
Kontsedal <kontsedalbohdan@gmail.com>
2020-03-27 12:17:54 +0200

    styling

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,19 +1,19 @@
 import React from "react";
-import classes from "./styles.module.scss";
+import s from "./styles.module.scss";
 import cn from "classnames";
 
 export const Loader = () => {
   return (
-    <div className={classes.loader}>
-      <div className={cn(classes.loaderCube, classes.loaderCube1)} />
-      <div className={cn(classes.loaderCube, classes.loaderCube2)} />
-      <div className={cn(classes.loaderCube, classes.loaderCube3)} />
-      <div className={cn(classes.loaderCube, classes.loaderCube4)} />
-      <div className={cn(classes.loaderCube, classes.loaderCube5)} />
-      <div className={cn(classes.loaderCube, classes.loaderCube6)} />
-      <div className={cn(classes.loaderCube, classes.loaderCube7)} />
-      <div className={cn(classes.loaderCube, classes.loaderCube8)} />
-      <div className={cn(classes.loaderCube, classes.loaderCube9)} />
+    <div className={s.loader}>
+      <div className={cn(s.loaderCube, s.loaderCube1)} />
+      <div className={cn(s.loaderCube, s.loaderCube2)} />
+      <div className={cn(s.loaderCube, s.loaderCube3)} />
+      <div className={cn(s.loaderCube, s.loaderCube4)} />
+      <div className={cn(s.loaderCube, s.loaderCube5)} />
+      <div className={cn(s.loaderCube, s.loaderCube6)} />
+      <div className={cn(s.loaderCube, s.loaderCube7)} />
+      <div className={cn(s.loaderCube, s.loaderCube8)} />
+      <div className={cn(s.loaderCube, s.loaderCube9)} />
     </div>
   );
 };
